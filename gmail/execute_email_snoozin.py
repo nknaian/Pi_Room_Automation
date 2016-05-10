ebf59d49f4448478da774fc74a1c80384eabec14
nknaian <nickknaian@gmail.com>
2016-05-10 16:32:14 -0400

    commit_message

--- gmail/execute_email_snoozin.py
+++ gmail/execute_email_snoozin.py
@@ -243,6 +243,7 @@ def poll_for_git_requests():
         
         proc = subprocess.Popen(["python3", "/home/pi/Desktop/Git_repo/Pi_Room_Automation/master_regulator.py"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
         out, err = proc.communicate()
+        err = ""
         if err!= "":
             subprocess.check_output(["python2", "/home/pi/Desktop/Git_repo/Pi_Room_Automation/gmail/execute_send_email.py", "email", "-v", "SendErrorMessage", "-b", err])
         else:

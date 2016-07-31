1a62a27a93ab4a43e836aab24a2343d13a59be74
nknaian <nickknaian@gmail.com>
2016-07-31 19:48:36 -0400

    Fixing string problem"

--- master_regulator.py
+++ master_regulator.py
@@ -318,7 +318,9 @@ while True:
         master_regulator()
     except subprocess.TimeoutExpired as error:
         print(error)
-        returnVal = run_send_email_and_monitor(["python2", "/home/pi/Desktop/Git_repo/Pi_Room_Automation/gmail/execute_send_email.py", "email", "-v", "SendWarningMessage", "-b", error])
+        err_str = str(error)
+        full_warning_message = err_str + "\n\nMaster Regulator has been restarted, so any flags you've set must now be reset..."
+        returnVal = run_send_email_and_monitor(["python2", "/home/pi/Desktop/Git_repo/Pi_Room_Automation/gmail/execute_send_email.py", "email", "-v", "SendWarningMessage", "-b", full_warning_message])
     except Exception as error:
         print(error)
         break

c71ff2514aba1e0ad3f6ba9b0d9408bb2f612110
nknaian <nickknaian@gmail.com>
2016-08-01 19:05:28 -0400

    syntax"

--- master_regulator.py
+++ master_regulator.py
@@ -325,7 +325,7 @@ while True:
         print(error)
         err_str = str(error)
         full_warning_message = err_str + "\n\nMaster Regulator has been restarted, so any flags you've set must now be reset..."
-        if "execute_send_email.py" not in full _warning_message:
+        if "execute_send_email.py" not in full_warning_message:
             print("\n\n\nThis better be a execute_email_snoozin timeout or else something's weird\n\n\n")
             returnVal = run_send_email_and_monitor(["python2", "/home/pi/Desktop/Git_repo/Pi_Room_Automation/gmail/execute_send_email.py", "email", "-v", "SendWarningMessage", "-b", full_warning_message])
     except Exception as error:

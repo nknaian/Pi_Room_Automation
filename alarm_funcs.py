0ebcd038751a404f321a1b7513fe80f5002e74fb
nknaian <nickknaian@gmail.com>
2016-08-06 17:47:35 -0400

    taking out general exception so that actual error message from the script will be printed"

--- alarm_funcs.py
+++ alarm_funcs.py
@@ -207,7 +207,9 @@ def play_youtube_video(url):
 
 def run_send_email_and_monitor(scriptWithArgs):
     out = subprocess.check_output(scriptWithArgs, stderr=subprocess.STDOUT, timeout = 60)
-    print(out)
+    str_out = str(out)
+    if str_out != "b\'\'":
+        print(out)
 
 
 def run_script_and_monitor(scriptWithArgs): #This function takes the script with args as a list, just as it would be typed in terminal...use to relay standard error and output (will not work for execute_send_email)

efef8d390ad8b6944ed4d86fbafb408d4b4e31a9
nknaian <nickknaian@gmail.com>
2016-12-03 20:45:18 -0500

    changing push script to cd into gmail before calling send email"

--- push_script.bat
+++ push_script.bat
@@ -4,5 +4,6 @@ git add -A
 set /p commit_message=Enter commit message (with quotations):
 git commit -m %commit_message%"
 git push
-python gmail\execute_send_email.py email -v GitPullRequest
+cd gmail
+python execute_send_email.py email -v GitPullRequest
 PAUSE

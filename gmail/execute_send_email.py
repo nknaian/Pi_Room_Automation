6b9aca1c67d9e81e9184f5f0abd46bf33522c6c3
nknaian <nickknaian@gmail.com>
2016-12-03 20:43:18 -0500

    ..."

--- gmail/execute_send_email.py
+++ gmail/execute_send_email.py
@@ -17,6 +17,9 @@ import send_email as mail
 
 try:
     import argparse
+
+    print ("Current folder: " + os.getcwd())
+
     flags = argparse.ArgumentParser(parents=[tools.argparser])#parse_args() <-- I changed this...change back if things are getting weird
     subparsers = flags.add_subparsers(help='prewritten emails to send')
     email_parser = subparsers.add_parser("email")
